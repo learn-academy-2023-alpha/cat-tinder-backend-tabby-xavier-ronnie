61f40913c0aaa142d4bb2f4bdc030bc70259e074
XavierB91 <xavierbarker183@gmail.com>
2023-04-04 16:16:42 -0700

    adds backend validations

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,10 @@
 class CatsController < ApplicationController
-end
+    def create
+        cat = Cat.create(cat_params)
+        if cat.valid?
+          render json: cat
+         else
+           render json: cat.errors, status: 422
+         end
+      end
+    end
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -1,7 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "cannot create a cat without valid attributes" do 
+    it "doesn't create a cat without a name" do 
+      cat_params = {
+        name: "Chai",
+        age: 3,
+        enjoys: "Chaos and Havok",
+        image: "https://images.unsplash.com/photo-1610878237189-81306e16bd89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80",
+      }
+      post '/cats', params: cat_params 
+      expect(response.status).to eq 422
+
+      cat = JSON.parse(response.body)
+      expect(json['name']).to include "can't be blank"
+    end
   end
-end
+    it "doesn't create a cat without an age" do 
+      cat_params = {
+        name: "Chai",
+        age: 3,
+        enjoys: "Chaos and Havok",
+        image: "https://images.unsplash.com/photo-1610878237189-81306e16bd89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80",
+        }
+
+      post '/cats', params: cat_params 
+      expect(response.status).to eq 422
+      cat = JSON.parse(response.body)
+      expect(json['age']).to include "can't be blank"
+    end
+
+    it "doesn't create a cat without an enjoys" do 
+      cat_params = {
+        name: "Chai",
+        age: 3,
+        enjoys: "Chaos and Havok",
+        image: "https://images.unsplash.com/photo-1610878237189-81306e16bd89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80",
+      }
+
+      post '/cats', params: cat_params 
+      expect(response.status).to eq 422
+      cat = JSON.parse(response.body)
+      expect(json['enjoys']).to include "can't be blank"
+    end
+
+    it "doesn't create a cat without an image" do 
+      cat_params = {
+        name: "Chai",
+        age: 3,
+        enjoys: "Chaos and Havok",
+        image: "https://images.unsplash.com/photo-1610878237189-81306e16bd89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80" 
+      }
+      post '/cats', params: cat_params 
+      expect(response.status).to eq 422
+      cat = JSON.parse(response.body)
+      expect(json['image']).to include "can't be blank"
+    end
+  end
+
+
