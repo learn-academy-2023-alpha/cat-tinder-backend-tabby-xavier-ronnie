8d9598cab518279ca8ffb272885711178c69d9cd
XavierB91 <xavierbarker183@gmail.com>
2023-04-04 11:54:38 -0700

    added Cat Tinder API Endpoints to Cat tinder Back end API

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,32 @@
 class CatsController < ApplicationController
+        def index
+            cats = Cat.all
+            render json: cats
+        end
+      
+        def create
+            # Create a new cat
+            cat = Cat.create(cat_params)
+            if cat.valid?
+                 render json: cat
+            end
+          end
+        
+        def update
+            cat = Cat.find(params[:id])
+            cat.update(cat_params)
+            if cat.valid?
+                render json: cat
+            end
+        end
+        def destroy
+            cat = Cat.find(params[:id])
+            cat.destroy
+            render json: cat
+        end
+          # Handle strong parameters, so we are secure
+          private
+          def cat_params
+            params.require(:cat).permit(:name, :age, :enjoys, :image)
+          end
 end
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -2,6 +2,76 @@ require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of cats" do
+      Cat.create(
+        name: "Chai",
+        age: 3,
+        enjoys: "Chaos and Havok",
+        image: "https://images.unsplash.com/photo-1610878237189-81306e16bd89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80",
+      )
+      get '/cats'
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(cat.length).to eq 1
+    end
   end
+  describe "POST/ create" do 
+    it "creates a cat" do
+      cat_params = {
+        cat: {  name: "Chai",
+          age: 3,
+          enjoys: "Chaos and Havok",
+          image: "https://images.unsplash.com/photo-1610878237189-81306e16bd89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80"
+        }
+      }
+      post '/cats', params: cat_params
+      expect(response).to have_http_status(200)
+      cat = Cat.first
+      expect(cat.name). to eq 'Chai'
+    end
+  end
+  describe "PATCH/update" do
+    it "updates a cat" do
+      cat_params = {
+        cat: {  
+          name: "Chai",
+          age: 3,
+          enjoys: "Chaos and Havok",
+          image: "https://images.unsplash.com/photo-1610878237189-81306e16bd89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80"
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+      updated_cat_params = {
+        cat: {  
+          name: "Chai",
+          age: 4,
+          enjoys: "Chaos and Havok",
+          image: "https://images.unsplash.com/photo-1610878237189-81306e16bd89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80"
+        }
+      } 
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      updated_cat = Cat.find(cat.id)
+      expect(response).to have_http_status(200)
+      expect(updated_cat.age).to eq 4
+    end
+  end
+    describe "DELETE/destroy" do
+      it "will delete a cat" do
+        cat_params = {
+          cat: {  
+            name: "Chai",
+            age: 3,
+            enjoys: "Chaos and Havok",
+            image: "https://images.unsplash.com/photo-1610878237189-81306e16bd89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80"
+          }
+        }
+        post '/cats', params:cat_params
+        cat = Cat.first
+        cats = Cat.all
+        delete "/cats/#{cat.id}"
+        expect(response).to have_http_status(200)
+        expect(cats).to be_empty 
+      end
+    end
 end
